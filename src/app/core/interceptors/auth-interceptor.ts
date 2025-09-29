a46954e49cf93c2b20f6d7fb4f32fb2933fd607c
Ranjithaa Ravichandran <ranjithaa1610@outlook.com>
2025-09-29 13:20:59 +0530

    On jwt access token expiry, navigating to login

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,12 +1,17 @@
 import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent, HttpClient } from '@angular/common/http';
-import { Observable, catchError, switchMap, throwError } from 'rxjs';
+import { EMPTY, Observable, catchError, switchMap, throwError } from 'rxjs';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Notificationservice } from '../../services/notificationservice';
 
 const API_BASE_URL = 'https://localhost:44313/Api/Auth/';
 
 export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
-
+ const http = inject(HttpClient);  
+  const router = inject(Router);
+  const notify = inject(Notificationservice);
   const isRefresh = req.url.includes('/refresh');
+  const isLogin = req.url.includes('/login');
   const token = localStorage.getItem('accessToken');
   console.log('Interceptor running', req.url, token);
 
@@ -19,8 +24,7 @@ export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: 
 
   return next(authReq).pipe(
     catchError(err => {
-      if (err.status === 401 && !isRefresh) {
-        const http = inject(HttpClient);
+      if (err.status === 401 && !isRefresh && !isLogin) {
         return http.post<{ accessToken: string }>(
           API_BASE_URL + 'refresh',
           {},
@@ -35,7 +39,16 @@ export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: 
           }),
           catchError(() => {
             localStorage.removeItem('accessToken');
-            return throwError(() => err);
+            notify.error('Session expired. Please log in again.');
+            // Decide redirect path based on current URL
+            const currentUrl = router.url.toLowerCase();
+            if (currentUrl.includes('/admindashboard')) {
+              router.navigate(['/adminlogin']);
+            } else {
+              router.navigate(['/startpage']);
+            }
+            // return throwError(() => err);
+            return EMPTY;
           })
         );
       }
