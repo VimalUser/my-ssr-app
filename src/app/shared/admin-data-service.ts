b551751afb0df43897654ef087441a1a42b1e7c2
Ranjithaa Ravichandran <ranjithaa1610@outlook.com>
2025-10-01 19:22:44 +0530

    Added new service to handle admin data

---
+++ src/app/shared/admin-data-service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { AdminData } from '../model/AdminData';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminDataService {
+  private dataSubject = new BehaviorSubject<AdminData>({
+    clientId: 0,
+    clientName: '',
+    status: '',
+    albumName: '',
+    albumDate: '',
+    tranditionalAlbumSelection: [],
+    candidAlbumSelection: [],
+    portraitFrameSelection: [],
+    landscapeFrameSelection: [],
+    coverSelection: [],
+    noOfPics: 0,
+    noOfFrames: 0,
+    coverPic: '',
+    mobileNumber: '',
+    eventTypeId: 0,
+    albumSizeId: 0,
+    frameSizeId: 0,
+    eventType: '',
+    albumSize: '',
+    frameSize: '',
+  });
+
+  // Observable to subscribe to data changes
+  data$ = this.dataSubject.asObservable();
+  
+  // Getter for current value
+  getData(): any {
+    return this.dataSubject.value;
+  }
+  // Update the entire data object
+  updateData(data: any) {
+    this.dataSubject.next(data);
+  }
+
+  patchData(patch: Partial<any>) {
+    const newData = { ...this.dataSubject.value, ...patch };
+    this.dataSubject.next(newData);
+  }
+
+}
