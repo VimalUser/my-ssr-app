da68877317ae7878ba74475747473a540fc1f1fd
Vimal <mvimalengineer@gmail.com>
2025-10-06 17:39:00 +0530

    comments screen added

--- src/app/component/admin-review-comments/admin-review-comments.ts
+++ src/app/component/admin-review-comments/admin-review-comments.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminDataService } from '../../shared/admin-data-service';
+import { AdminData } from '../../model/AdminData';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-review-comments',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './admin-review-comments.html',
   styleUrl: './admin-review-comments.css'
 })
-export class AdminReviewComments {
+export class AdminReviewComments implements OnInit {
 
+  constructor(
+    private adminDataService :AdminDataService
+  ){}
+  pageLatestAdminData: AdminData = {} as AdminData;
+
+  ngOnInit(): void {
+
+     this.adminDataService.data$.subscribe(data => {
+      if (data) {
+        this.pageLatestAdminData = data;
+       
+        console.log("Traditional count:", this.pageLatestAdminData);
+    }
+    });
+  }
+
+  get PortraitFilename(){
+   return this.pageLatestAdminData.portraitFrameSelection[0].fileName || '';
+  }
+  get PortraitComment(){
+   return this.pageLatestAdminData.portraitFrameSelection[0].comment || '';
+  }
+
+   get LandscapeFileName(){
+   return this.pageLatestAdminData.portraitFrameSelection[0].fileName || '';
+  }
+  get LandscapeComment(){
+   return this.pageLatestAdminData.portraitFrameSelection[0].comment || '';
+  }
 }
