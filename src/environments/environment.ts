47fd58151c8936ab55985eac6f7964ab60d51a1a
Vimal <mvimalengineer@gmail.com>
2025-10-11 00:13:53 +0530

    Latest changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,18 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://apicandyimagesdev.azurewebsites.net',
-  authUrl: 'https://apicandyimagesdev.azurewebsites.net/api/auth/',
-  clientAlbumUrl: 'https://apicandyimagesdev.azurewebsites.net/Api/ClientAlbum/',
-  blobUrl: 'https://apicandyimagesdev.azurewebsites.net/Api/Blob/',
-  allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
-};
-
 // export const environment = {
-//   production: false,
-//   apiUrl: 'https://perfectlypickedapi.azurewebsites.net/perfectlypicked',
-//   authUrl: 'https://localhost:44313/Api/Auth/',
-//   clientAlbumUrl: 'https://localhost:44313/Api/ClientAlbum/',
-//   blobUrl: 'https://localhost:44313/Api/Blob/',
+//   production: true,
+//   apiUrl: 'https://apicandyimagesdev.azurewebsites.net',
+//   authUrl: 'https://apicandyimagesdev.azurewebsites.net/api/auth/',
+//   clientAlbumUrl: 'https://apicandyimagesdev.azurewebsites.net/Api/ClientAlbum/',
+//   blobUrl: 'https://apicandyimagesdev.azurewebsites.net/Api/Blob/',
 //   allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
 // };
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://perfectlypickedapi.azurewebsites.net/perfectlypicked',
+  authUrl: 'https://localhost:7112/Api/Auth/',
+  clientAlbumUrl: 'https://localhost:7112/Api/ClientAlbum/',
+  blobUrl: 'https://localhost:7112/Api/Blob/',
+  allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
+};
 
