f5b4dfec2b4c1e287d610e9a35b4f51dfa31f927
Ranjithaa Ravichandran <ranjithaa1610@outlook.com>
2025-11-25 18:55:39 +0530

    Changed angular file to build the ui app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,17 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://apicandyimagesdev.azurewebsites.net',
-//   authUrl: 'https://apicandyimagesdev.azurewebsites.net/api/auth/',
-//   clientAlbumUrl: 'https://apicandyimagesdev.azurewebsites.net/Api/ClientAlbum/',
-//   blobUrl: 'https://apicandyimagesdev.azurewebsites.net/Api/Blob/',
-//   allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
-// };
-
 export const environment = {
-  production: false,
-  apiUrl: 'https://perfectlypickedapi.azurewebsites.net/perfectlypicked',
-  authUrl: 'https://localhost:7112/Api/Auth/',
-  clientAlbumUrl: 'https://localhost:7112/Api/ClientAlbum/',
-  blobUrl: 'https://localhost:7112/Api/Blob/',
+  production: true,
+  authUrl: '/api/auth/',
+  clientAlbumUrl: '/Api/ClientAlbum/',
+  blobUrl: '/Api/Blob/',
   allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://perfectlypickedapi.azurewebsites.net/perfectlypicked',
+//   authUrl: 'https://localhost:7112/Api/Auth/',
+//   clientAlbumUrl: 'https://localhost:7112/Api/ClientAlbum/',
+//   blobUrl: 'https://localhost:7112/Api/Blob/',
+//   allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
+// };
 
