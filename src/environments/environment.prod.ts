54bdc8f75eeee509877a0891a996721b69e1f564
Vimal <mvimalengineer@gmail.com>
2025-11-26 18:39:08 +0530

    added production environment file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  authUrl: '/api/auth/',
+  clientAlbumUrl: '/Api/ClientAlbum/',
+  blobUrl: '/Api/Blob/',
+  allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,10 @@
+
 export const environment = {
-  production: true,
-  authUrl: '/api/auth/',
-  clientAlbumUrl: '/Api/ClientAlbum/',
-  blobUrl: '/Api/Blob/',
+  production: false,
+  apiUrl: 'https://perfectlypickedapi.azurewebsites.net/perfectlypicked',
+  authUrl: 'https://localhost:7112/Api/Auth/',
+  clientAlbumUrl: 'https://localhost:7112/Api/ClientAlbum/',
+  blobUrl: 'https://localhost:7112/Api/Blob/',
   allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
 };
-
-// export const environment = {
-//   production: false,
-//   apiUrl: 'https://perfectlypickedapi.azurewebsites.net/perfectlypicked',
-//   authUrl: 'https://localhost:7112/Api/Auth/',
-//   clientAlbumUrl: 'https://localhost:7112/Api/ClientAlbum/',
-//   blobUrl: 'https://localhost:7112/Api/Blob/',
-//   allowedImageTypes: ['.jpg', '.jpeg', '.png', '.gif']
-// };
 
